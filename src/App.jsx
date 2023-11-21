d0b405fd0d7cb564ca613e563a9e2ce8752349ae
unknown <1803684g@umich.mx>
2023-11-20 22:55:29 -0600

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import Header from './components/Header'
-import NavBar from './components/NavBar'
+import React from 'react';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Test from './components/Test';
 
 const App = () => {
   return (
-    <div>
+    <Router>
+
       <div>
         <NavBar />
       </div>
       <div>
-        <Header />
+        
       </div>
-    </div>
+      <Routes >
+        <Route path="/" element={<Header />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+
+    </Router>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import s from './Header.module.css';
-import tre from './../assets/tre.jpg'
+import tre from './../assets/tre.jpg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  console.log(React.version);
   return (
     <div className={s.container} > 
 
@@ -13,6 +15,8 @@ const Header = () => {
         <div className={s.tex}>
             <h2>Subtitle</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum iusto error accusamus natus tenetur veniam voluptatibus dolorum autem porro, eveniet animi dicta repellendus hic nobis magnam perspiciatis repudiandae quo itaque?</p>
+            
+            <Link to="/test">Start</Link>
         </div>
 
     </div>
